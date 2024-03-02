8564cb528769d3d08885ab3e7194eb4eb8809f7f
sifat-fahimul <mdsifatfeni@gmail.com>
2024-03-02 09:58:32 +0600

    error handler update mongo to prisma

--- src/app/modules/semesterRegister/semesterRegister.route.ts
+++ src/app/modules/semesterRegister/semesterRegister.route.ts
@@ -19,13 +19,13 @@ router.post(
 router.patch(
   '/:id',
   validateRequest(SemesterRegistrationValidation.update),
-  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  // auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   SemesterRegisterController.updateIntoDB
 );
 
 router.delete(
   '/:id',
-  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  // auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   SemesterRegisterController.deleteFromDB
 );
 
